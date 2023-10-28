2be6c56f0fa129df2962792062ac8e6059748363
woojae9488 <woojae9488@naver.com>
2023-10-28 18:25:44 +0900

    init project

---
+++ src/main/java/com/kwj/spring/kafka/oncekafkaconsumer/OnceKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.kwj.spring.kafka.oncekafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnceKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnceKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwj/spring/kafka/oncekafkaconsumer/OnceKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwj.spring.kafka.oncekafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnceKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
